c635485f59c80cbe1778751b74b1ecca7d3087d4
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2024-02-19 10:51:04 +0200

    treewide: Unify formatting
    Use clang-format on all c++ files.

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,27 @@
 #include <QtQuick/QQuickView>
 #include <QTranslator>
 
-int main(int argc, char *argv[]) {
-    QGuiApplication app(argc, argv);
+int main(int argc, char *argv[])
+{
+	QGuiApplication app(argc, argv);
 
-    QString locale = QLocale::system().name();
-    QTranslator translator;
-    bool translated = translator.load(
-        QString(":/i18n/cutie-phone_") + locale);
-    if (translated)
-        app.installTranslator(&translator);
+	QString locale = QLocale::system().name();
+	QTranslator translator;
+	bool translated =
+		translator.load(QString(":/i18n/cutie-phone_") + locale);
+	if (translated)
+		app.installTranslator(&translator);
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(
-        &engine, &QQmlApplicationEngine::objectCreated, &app,
-        [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-        },
-        Qt::QueuedConnection);
-    engine.load(url);
+	QQmlApplicationEngine engine;
+	const QUrl url(QStringLiteral("qrc:/main.qml"));
+	QObject::connect(
+		&engine, &QQmlApplicationEngine::objectCreated, &app,
+		[url](QObject *obj, const QUrl &objUrl) {
+			if (!obj && url == objUrl)
+				QCoreApplication::exit(-1);
+		},
+		Qt::QueuedConnection);
+	engine.load(url);
 
-    return app.exec();
+	return app.exec();
 }
